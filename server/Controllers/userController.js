5d5de2369748c2c5b65236d216fbf55a4153979a
juda-star <juda.ishta@gmail.com>
2021-08-11 23:56:28 +0300

    if

--- server/Controllers/userController.js
+++ server/Controllers/userController.js
@@ -3,6 +3,9 @@ const bcryptjs= require("bcryptjs");
 
 async function register() {
 await studentModel.findOne({email:require.body.studentModel.email},(err,student)=>{
+    if (student) {
+        return res.status(400).json({email:"email already exists"});
+    }
     
 })
     
