e512f335d31baa398ff4202e6580652b6080629b
juda-star <juda.ishta@gmail.com>
2021-08-11 23:57:20 +0300

    function done

--- server/Controllers/userController.js
+++ server/Controllers/userController.js
@@ -6,7 +6,22 @@ await studentModel.findOne({email:require.body.studentModel.email},(err,student)
     if (student) {
         return res.status(400).json({email:"email already exists"});
     }
-    
+    else{
+        bcryptjs.genSalt(10,(err,salt)=>{
+            bcryptjs.hash(req.body.student.password,salt,(err,hash)=>{
+                if (err) throw err;
+                req.body.student.password=hash;
+                studentModel.insertMany(req.body.student,(err)=>{
+                    if (err) {
+                        return res.status(400).json({success:false,error:err})
+                    };
+                    res.status(201).json({success:true,massage:`registered of ${req.body.student} is confirmed`})
+                })
+                    
+                
+            })
+        })
+    }
 })
     
 }
